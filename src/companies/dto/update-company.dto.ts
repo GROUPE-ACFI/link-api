d1adbba02263f93f3a316de3b5c3d79072a3fa08
Claire Pelletier <cpelletier@acfi-associes.com>
2025-08-05 15:15:16 +0200

    fix(companies): add address DTOs

--- src/companies/dto/update-company.dto.ts
+++ src/companies/dto/update-company.dto.ts
@@ -2,13 +2,13 @@ import { PartialType, ApiPropertyOptional } from '@nestjs/swagger';
 import { CreateCompanyDto } from './create-company.dto';
 import { IsOptional, ValidateNested, IsArray } from 'class-validator';
 import { Type } from 'class-transformer';
-import { AddressDto } from './address.dto';
+import { UpdateAddressDto } from './update-address.dto';
 
 export class UpdateCompanyDto extends PartialType(CreateCompanyDto) {
-  @ApiPropertyOptional({ type: () => [AddressDto] })
+  @ApiPropertyOptional({ type: () => [UpdateAddressDto] })
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
-  @Type(() => AddressDto)
-  addresses?: AddressDto[];
+  @Type(() => UpdateAddressDto)
+  addresses?: UpdateAddressDto[];
 }
