46e063fee377781e19658c943f3de0f88482cb11
Claire Pelletier <cpelletier@acfi-associes.com>
2025-08-05 15:26:14 +0200

    fix(companies): allow update addresses

--- src/companies/dto/update-company.dto.ts
+++ src/companies/dto/update-company.dto.ts
@@ -1,10 +1,12 @@
-import { PartialType, ApiPropertyOptional } from '@nestjs/swagger';
+import { PartialType, ApiPropertyOptional, OmitType } from '@nestjs/swagger';
 import { CreateCompanyDto } from './create-company.dto';
 import { IsOptional, ValidateNested, IsArray } from 'class-validator';
 import { Type } from 'class-transformer';
 import { UpdateAddressDto } from './update-address.dto';
 
-export class UpdateCompanyDto extends PartialType(CreateCompanyDto) {
+export class UpdateCompanyDto extends PartialType(
+  OmitType(CreateCompanyDto, ['addresses'] as const),
+) {
   @ApiPropertyOptional({ type: () => [UpdateAddressDto] })
   @IsOptional()
   @IsArray()
